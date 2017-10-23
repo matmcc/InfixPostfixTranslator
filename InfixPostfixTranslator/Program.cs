714bf385f193cff6bb9d3e65348565b439bba33b
matmcc <matmcconkey@gmail.com>
2017-10-23 20:59:02 +0100

    Added TODO comments to lay out future jobs

--- InfixPostfixTranslator/Program.cs
+++ InfixPostfixTranslator/Program.cs
@@ -4,6 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//  TODO: Kepp in separate files?
+//  TODO: Resolve issue re: symbols in Expression class constructor
+
+//  TODO: Redesign InfixToPostfix.Precedence ?
+//  TODO: Is everything as private as possible?
+//  TODO: Are scopes as minimal as is practical?
+
+//  TODO: Learn how to, then write unit tests
+//  TODO: Learn how to, then write xml tagged comments/documentation
+
+//  TODO: Is this structured as it should be?
+//  TODO: Create class diagrams
+
+//  TODO: Are all control-flow designed to exit early in the best way? i.e. most likely bool-test first?
+
 namespace InfixPostfixTranslator
 {
     class Program
